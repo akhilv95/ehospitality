f53fceaa5e517d4f7964b0525e1569157455deba
akhil valanky <akhilvalankyofficial@gmail.com>
2026-06-29 14:26:28 +0530

    Add production environment configuration

--- config/config/settings.py
+++ config/config/settings.py
@@ -126,12 +126,13 @@ SIMPLE_JWT = {
 }
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:5173",
+#     "http://localhost:5173",
+# ]
+CORS_ALLOWED_ALL_ORIGINS=True
 CORS_ALLOW_CREDENTIALS = True
 
 # API Documentation
